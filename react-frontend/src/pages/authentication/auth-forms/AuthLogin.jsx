37c002be9ef2476ca4bcbac0f33292a694df65d3
Salar Satti <salarsatti@salars-mbp-2.lan>
2024-06-15 00:18:48 -0400

    Update the front-end to support a login screen

--- react-frontend/src/pages/authentication/auth-forms/AuthLogin.jsx
+++ react-frontend/src/pages/authentication/auth-forms/AuthLogin.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
+import axios from 'axios';
 
 // material-ui
 import Button from '@mui/material/Button';
@@ -16,6 +17,9 @@ import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import LinearProgress from '@mui/material/LinearProgress';
+import { useNavigate } from 'react-router-dom';
+
 
 // third party
 import * as Yup from 'yup';
@@ -27,12 +31,23 @@ import AnimateButton from 'components/@extended/AnimateButton';
 // assets
 import EyeOutlined from '@ant-design/icons/EyeOutlined';
 import EyeInvisibleOutlined from '@ant-design/icons/EyeInvisibleOutlined';
+
 import FirebaseSocial from './FirebaseSocial';
+import { set } from 'lodash';
 
 // ============================|| JWT - LOGIN ||============================ //
 
 export default function AuthLogin({ isDemo = false }) {
   const [checked, setChecked] = React.useState(false);
+
+  // form validation hooks
+  const [showLoadIcon, setShowLoadIcon] = React.useState(false);
+  const [showInvalidError, setShowInvalidError] = React.useState(false);
+  const [isSub, setIsSub] = React.useState(false);
+  const [showUnkownError, setShowUnkownError] = React.useState(false);
+
+  // react-router navigate hook
+  const navigate = useNavigate();
 
   const [showPassword, setShowPassword] = React.useState(false);
   const handleClickShowPassword = () => {
@@ -43,18 +58,58 @@ export default function AuthLogin({ isDemo = false }) {
     event.preventDefault();
   };
 
+  const wait = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
+
+  const handleSubmit = (values) => {
+    
+    console.log(values);
+    const waitForSeconds = async () => {
+      setIsSub(true);
+      setShowInvalidError(false);
+      setShowLoadIcon(true);
+      await wait(1000); // Wait for 1 seconds
+      axios.post('/api/auth/login', {'email': values.email, 'password': values.password})
+        .then(response => {
+          console.log(response);
+          console.log(response.status === 200)
+          // navigate to the home page
+          navigate('/');
+        })
+        .catch(error => {
+          console.log(error);
+          // represent and invalid email and password
+          if (error.response.status === 401) {
+            // show error message
+            setShowInvalidError(true);
+          // something else has gone wrong
+          } else {
+            setShowUnkownError(true);
+          }
+          // allow to submit again
+          setIsSub(false);
+          
+        });
+      setShowLoadIcon(false);
+    };
+    waitForSeconds();
+
+  }
+
   return (
     <>
       <Formik
         initialValues={{
-          email: '',
-          password: '',
+          email: 'salarsattiss@gmail.com',
+          password: 'Stongassword12345!',
           submit: null
         }}
         validationSchema={Yup.object().shape({
           email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
           password: Yup.string().max(255).required('Password is required')
         })}
+        onSubmit={handleSubmit}
       >
         {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
           <form noValidate onSubmit={handleSubmit}>
@@ -115,7 +170,7 @@ export default function AuthLogin({ isDemo = false }) {
                 )}
               </Grid>
 
-              <Grid item xs={12} sx={{ mt: -1 }}>
+              {/* <Grid item xs={12} sx={{ mt: -1 }}>
                 <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
                   <FormControlLabel
                     control={
@@ -127,33 +182,59 @@ export default function AuthLogin({ isDemo = false }) {
                         size="small"
                       />
                     }
-                    label={<Typography variant="h6">Keep me sign in</Typography>}
+                    label={<Typography variant="h6">Keep me signed in</Typography>}
                   />
                   <Link variant="h6" component={RouterLink} color="text.primary">
                     Forgot Password?
                   </Link>
                 </Stack>
-              </Grid>
+              </Grid> */}
               {errors.submit && (
                 <Grid item xs={12}>
                   <FormHelperText error>{errors.submit}</FormHelperText>
                 </Grid>
               )}
+              {showInvalidError ?
+              <Grid item xs={12}>
+                <Typography color='red'>
+                  This email and password does not exist.
+                </Typography>
+              </Grid>
+              : null}
+
+              {showUnkownError ?
+              <Grid item xs={12}>
+                <Typography color='red'>
+                  Something went wrong. Please try again later.
+                </Typography>
+              </Grid>
+              : null}
+
               <Grid item xs={12}>
                 <AnimateButton>
-                  <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="primary">
+                  <Button disableElevation disabled={isSub} fullWidth size="large" type="submit" variant="contained" color="primary">
                     Login
                   </Button>
                 </AnimateButton>
               </Grid>
-              <Grid item xs={12}>
+
+              {/* <Grid item xs={12}>
                 <Divider>
                   <Typography variant="caption"> Login with</Typography>
                 </Divider>
               </Grid>
               <Grid item xs={12}>
                 <FirebaseSocial />
+              </Grid> */}
+              
+              {showLoadIcon ? 
+              <Grid item xs={12}>
+                <LinearProgress />
               </Grid>
+              :
+              null}
+             
+
             </Grid>
           </form>
         )}

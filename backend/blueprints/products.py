b83d044f305b7142d24d667befe6e94934e2e66f
Salar Satti <salarsatti@mac.lan>
2025-04-03 22:10:59 -0400

    Create Mongo Interface Class and Pydantic Validation Class for products

--- backend/blueprints/products.py
+++ backend/blueprints/products.py
@@ -1,6 +1,42 @@
 from fastapi import APIRouter, Request
 from backend.services.product import ProductService
 
+
+from pydantic import BaseModel, Field, HttpUrl
+from typing import List, Optional, Literal
+
+
+class ProductTableItem(BaseModel):
+    category: str
+    why_this_one: List[str]
+    price: str
+    link: str  # Consider using HttpUrl if you want URL validation
+
+
+class WhatToLookFor(BaseModel):
+    description: str
+    no_brainers: List[str]
+    things_to_think_about: List[str]
+
+
+class ProductDetails(BaseModel):
+    """
+    This is the format of the data that is expected to be sent to the API.
+    """
+    title: str
+    category: str
+    price_estimate: str
+    usefulness_knee_hip_surgery: Literal["low", "medium", "high"]
+    usefulness_stroke_and_brain_injury: Literal["low", "medium", "high"]
+    usefulness_aging_in_place: Literal["low", "medium", "high"]
+    
+    initial_description: str
+    product_table: List[ProductTableItem]
+    what_to_look_for: WhatToLookFor
+    more_to_consider: str
+
+
+
 def create_product_router(product_service: ProductService):
     product_router = APIRouter()
 
